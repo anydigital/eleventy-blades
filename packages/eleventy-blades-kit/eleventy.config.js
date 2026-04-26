fdbeb1eac3e24d75b4c5a9c10e2f339aebb64c94
Anton Staroverov <73071319+tonystar@users.noreply.github.com>
2026-04-25 19:35:33 -0600

    Please provide the changes or a description of the files you modified so I can generate the commit message for you.

--- packages/eleventy-blades-kit/eleventy.config.js
+++ packages/eleventy-blades-kit/eleventy.config.js
@@ -2,7 +2,7 @@ import baseConfig from "@anyblades/eleventy-blades/base-config";
 
 export default function (eleventyConfig) {
   eleventyConfig.setInputDirectory("../");
-  eleventyConfig.setIncludesDirectory(".bladeswitch/_includes/");
+  eleventyConfig.setIncludesDirectory(".blades/_includes/");
   eleventyConfig.setOutputDirectory("../_site/");
 
   baseConfig(eleventyConfig);
